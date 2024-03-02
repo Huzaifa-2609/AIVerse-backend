10e792755750a48878dc52aa43219800729c14d8
Syed Haseeb Ahmed <haseeb.ahmed@ephlux.com>
2024-03-02 18:32:28 +0500

    sagemaker implementation

--- src/controllers/modelhost.controller.js
+++ src/controllers/modelhost.controller.js
@@ -6,8 +6,54 @@ const config = require('./../config/config')
 const Docker = require('dockerode');
 const tar = require('tar')
 const path = require('path');
-const { exec, spawn } = require('child_process');
+const { exec } = require('child_process');
 
+
+
+const hostModelToSageMaker = async (s3Filename, imageName) => {
+    try {
+        const sm = new AWS.SageMaker();
+        let s3URL = `s3://aiversebucket/${s3Filename}`
+        let dockerImage = `975050334693.dkr.ecr.us-east-2.amazonaws.com/aiverseecr:${imageName}`
+        // let s3URL = `s3://aiversebucket1/model-1709363942814.tar.gz`
+        // let dockerImage = `975050334693.dkr.ecr.us-east-2.amazonaws.com/aiverseecr:model-1709363942814.tar.gz-1709363943793`
+        let modelName = "AIVERSE-MODEL"
+        const createModelParams = {
+            ModelName: modelName,
+            PrimaryContainer: {
+                Image: dockerImage,
+                ModelDataUrl: s3URL
+            },
+            ExecutionRoleArn: 'arn:aws:iam::975050334693:role/AIVERSE@123'
+        };
+        const modelResult = await sm.createModel(createModelParams).promise();
+        console.log('Model created:', modelResult);
+
+        const createEndpointConfigParams = {
+            EndpointConfigName: `${modelName}-config`,
+            ProductionVariants: [{
+                VariantName: 'default-variant',
+                ModelName: modelName,
+                InitialInstanceCount: 1,
+                InstanceType: 'ml.t2.medium'
+            }]
+        };
+        const endpointConfigResult = await sm.createEndpointConfig(createEndpointConfigParams).promise();
+        console.log('Endpoint config created:', endpointConfigResult);
+
+        const createEndpointParams = {
+            EndpointName: `${modelName}-endpoint`,
+            EndpointConfigName: `${modelName}-config`
+        };
+        const endpointResult = await sm.createEndpoint(createEndpointParams).promise();
+        console.log('Endpoint created:', endpointResult);
+
+        return endpointResult.EndpointArn;
+    } catch (error) {
+        console.error('Error deploying model:', error);
+        throw error;
+    }
+}
 
 const createDockerImage = async (filepath, req, res, imageName) => {
     try {
@@ -53,7 +99,6 @@ const createDockerImage = async (filepath, req, res, imageName) => {
                     } else {
                         console.log("Image built successfully:", output);
 
-                        let imageId;
                         for (let i = output.length - 1; i >= 0; i--) {
                             let element = output[i];
                             if (element.aux && element.aux.ID) {
@@ -71,6 +116,7 @@ const createDockerImage = async (filepath, req, res, imageName) => {
                                 return;
                             }
                             console.log(`Login command: ${stdout}`);
+
                             let tagCommand = `docker tag ${imageName} 975050334693.dkr.ecr.us-east-2.amazonaws.com/aiverseecr:${imageName}`
                             exec(tagCommand, (error, stdout, stderr) => {
                                 if (error) {
@@ -80,13 +126,15 @@ const createDockerImage = async (filepath, req, res, imageName) => {
                                 }
                                 console.log(`Tag command: ${stdout}`);
                                 let pushCommand = `docker push 975050334693.dkr.ecr.us-east-2.amazonaws.com/aiverseecr:${imageName}`
-                                exec(pushCommand, (error, stdout, stderr) => {
+                                exec(pushCommand, async (error, stdout, stderr) => {
                                     if (error) {
                                         console.error('Error getting Push command:', error);
                                         deleteFolder(normalPath)
                                         return;
+                                    } else {
+                                        console.log(`Push command: ${stdout}`);
+                                        await hostModelToSageMaker(req.file.filename, imageName)
                                     }
-                                    console.log(`Push command: ${stdout}`);
                                     deleteFolder(normalPath)
                                 })
                             })
@@ -109,7 +157,7 @@ const uploadToS3 = async (req, filePath) => {
     const s3 = new AWS.S3();
     const uploadParams = {
         Bucket: config.aws.bucketName,
-        Key: `${req.file.filename}.tar.gz`,
+        Key: `${req.file.filename}`,
         Body: fs.readFileSync(filePath)
     };
     await s3.upload(uploadParams).promise();
@@ -153,8 +201,8 @@ exports.hostModelToAWS = async (req, res) => {
         // const files = [req.file.filename];
         // await tar.c({ gzip: true, file: archivePath, cwd: destination }, files);
 
-        //upload tar file to s3 bucket
-        // await uploadToS3(req, filePath)
+        // upload tar file to s3 bucket
+        await uploadToS3(req, filePath)
 
         //build docker image
         // let imageUri = `aiverseecr:${req.file.filename}-${Date.now()}`
